937a690114551cd93248a483285209cf0d159d5a
Xuzhou Zheng <zhengxuzhou18@mails.ucas.edu.cn>
2020-10-17 15:09:10 +0000

    acceptance passed lab1 backup(no-relocation, with-big-kernel-loading)

--- include/os/sched.h
+++ include/os/sched.h
@@ -2,10 +2,10 @@
  *            Copyright (C) 2018 Institute of Computing Technology, CAS
  *               Author : Han Shukai (email : hanshukai@ict.ac.cn)
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
- *        Process scheduling related content, such as: scheduler, process blocking, 
+ *        Process scheduling related content, such as: scheduler, process blocking,
  *                 process wakeup, process creation, process kill, etc.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -22,8 +22,8 @@
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE. 
- * 
+ * THE SOFTWARE.
+ *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * */
 
 #ifndef INCLUDE_SCHEDULER_H_
@@ -37,61 +37,54 @@
 /* used to save register infomation */
 typedef struct regs_context
 {
-    
 
 } regs_context_t; /* 256 + 56 = 312B */
 
 typedef enum
 {
-    TASK_BLOCKED,
-    TASK_RUNNING,
-    TASK_READY,
-    TASK_EXITED,
+    TASK_BLOCKED, // 0
+    TASK_RUNNING, // 1
+    TASK_READY,   // 2
+    TASK_EXITED,  // 3
 } task_status_t;
 
 typedef enum
 {
-    KERNEL_PROCESS,
-    KERNEL_THREAD,
-    USER_PROCESS,
-    USER_THREAD,
+    KERNEL_PROCESS, // 0
+    KERNEL_THREAD,  // 1
+    USER_PROCESS,   // 2
+    USER_THREAD,    // 3
 } task_type_t;
 
 /* Process Control Block */
 typedef struct pcb
 {
     /* register context */
-    
 
     /* previous, next pointer */
-     
+    struct pcb *prev, *next;
     /* task in which queue */
-    
 
-    /* What tasks are blocked by me, the tasks in this 
-     * queue need to be unblocked when I do_exit(). */
-     
+    /*
+     * What tasks are blocked by me, the tasks in this
+     * queue need to be unblocked when I do_exit().
+     */
 
     /* holding lock */
-     
 
     /* block related */
-    
 
     /* priority */
-     
-
+    uint32_t prior;
     // name
-     
 
     /* process id */
-    
-    /* kernel/user thread/process */
-     
-    /* BLOCK | READY | RUNNING */
-     
+
+    /* task type: kernel/user thread/process */
+    task_type_t tsk_tp;
+    /* task status: BLOCK | READY | RUNNING */
+    task_status_t tsk_sts;
     /* cursor position */
-    
 
 } pcb_t;
 
@@ -104,10 +97,10 @@ typedef struct task_info
 } task_info_t;
 
 /* ready queue to run */
-extern queue_t ready_queue ;
+extern queue_t ready_queue;
 
 /* block queue to wait */
-extern queue_t block_queue ;
+extern queue_t block_queue;
 
 /* current running task PCB */
 extern pcb_t *current_running;
--- init/main.c
+++ init/main.c
@@ -4,11 +4,11 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
  *         The kernel's entry, where most of the initialization work is done.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
- * software and associated documentation files (the "Software"), to deal in the Software 
- * without restriction, including without limitation the rights to use, copy, modify, 
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
  * persons to whom the Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in
@@ -20,8 +20,8 @@
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE. 
- * 
+ * THE SOFTWARE.
+ *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * */
 
 #include "fs.h"
@@ -110,6 +110,7 @@ void __attribute__((section(".entry_function"))) _start(void)
 
     while (1)
     {
+        do_scheduler(); // prj2-1
     };
     return;
 }
